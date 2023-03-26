7b9ff83ae6bafb2a6756a98e1a054150fccf3527
girish4016 <115746976+girish4016@users.noreply.github.com>
2023-03-26 23:19:29 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+// function inc(){
+//     let k = document.querySelector('.num');
+//     let number = parseInt(k.textContent);
+//     number++;
+//     k.textContent = number;
+// }
+
+// function dic(){
+//     let k = document.querySelector('.num');
+//     let number = parseInt(k.textContent);
+//     number--;
+//     k.textContent = number;
+// }
+
